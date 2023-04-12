7b1bcbe32d55a5f6bc689362072e59471db32c78
Sowndarya9920 <sowndaryasakthi66@gmail.com>
2023-04-12 19:03:47 +0530

    commit

---
+++ src/main/java/com/geekster/ChuckNorris/ChuckNorrisApplication.java
@@ -0,0 +1,41 @@
+package com.geekster.ChuckNorris;
+
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class ChuckNorrisApplication {
+
+	public static void main(String[] args) throws Exception{
+
+		URL getUrl = new URL("https://api.chucknorris.io/jokes/random");
+		HttpURLConnection connection = (HttpURLConnection) getUrl.openConnection();
+		connection.setRequestMethod("GET");
+
+
+		int responseCode = connection.getResponseCode();
+
+		if (responseCode == 200) {
+
+
+			BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			StringBuilder jsonResponseData = new StringBuilder();
+			String readLine = null;
+
+
+			while ((readLine = in.readLine()) != null) {
+				jsonResponseData.append(readLine);
+			}
+
+			in.close();
+			System.out.println(jsonResponseData.toString());
+		} else {
+			System.out.println("This is not valid URL- " + responseCode);
+		}
+
+	}
+}
